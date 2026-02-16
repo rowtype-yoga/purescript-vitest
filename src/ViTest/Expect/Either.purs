caacf7656ca1f1acf923ada11257950d6b72a538
Mark <mark.eibes@gmail.com>
2026-02-16 23:48:19 +0100

    Prepare for publish v2.0.1

--- src/ViTest/Expect/Either.purs
+++ src/ViTest/Expect/Either.purs
@@ -2,7 +2,7 @@ module ViTest.Expect.Either where
 
 import Prelude
 
-import Data.Either (Either(..), either)
+import Data.Either (Either(..))
 import Effect.Aff (Aff)
 import ViTest.Expect (expectToBe, expectToEqual)
 
@@ -10,25 +10,25 @@ import ViTest.Expect (expectToBe, expectToEqual)
 expectRight :: forall e a. Eq a => Show e => a -> Either e a -> Aff Unit
 expectRight expected result = case result of
   Right actual -> expectToEqual expected actual
-  Left err -> expectToBe true false -- Always fails, shows error in output
+  Left _ -> expectToBe true false -- Always fails, shows error in output
 
 -- | Assert that a value is Right (don't care about the value)
 expectIsRight :: forall e a. Show e => Either e a -> Aff Unit
 expectIsRight result = case result of
   Right _ -> expectToBe true true
-  Left err -> expectToBe true false
+  Left _ -> expectToBe true false
 
 -- | Assert that a value is Left with a specific error
 expectLeft :: forall e a. Eq e => Show a => e -> Either e a -> Aff Unit
 expectLeft expected result = case result of
   Left actual -> expectToEqual expected actual
-  Right val -> expectToBe true false
+  Right _ -> expectToBe true false
 
 -- | Assert that a value is Left (don't care about the error)
 expectIsLeft :: forall e a. Show a => Either e a -> Aff Unit
 expectIsLeft result = case result of
   Left _ -> expectToBe true true
-  Right val -> expectToBe true false
+  Right _ -> expectToBe true false
 
 -- | Assert that a Left contains a specific substring in its error
 expectLeftContains :: forall a. Show a => String -> Either String a -> Aff Unit
@@ -37,7 +37,7 @@ expectLeftContains needle result = case result of
     if contains needle err
     then expectToBe true true
     else expectToBe true false -- Fails with message about not containing
-  Right val -> expectToBe true false
+  Right _ -> expectToBe true false
   where
     contains :: String -> String -> Boolean
     contains n h = indexOf n h >= 0
